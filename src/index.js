234387f97c882075f761a6c6feaf054c9b85d5f7
Desktop Casa <ander-reis@hotmail.com>
2019-12-01 16:57:21 -0300

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,34 @@
+const {GraphQLServer} = require('graphql-yoga')
+const Binding = require('prisma-binding')
+const {prisma} = require('./generated/prisma-client')
+
+const binding = new Binding.Prisma({
+   typeDefs: `${__dirname}/generated/graphql-schema/prisma.graphql`,
+   endpoint: process.env.PRISMA_ENDPOINT
+});
+
+const resolvers = {
+    Query: {
+        user(parent, args, context, info) {
+            // return prisma.user({id: args.id})
+            // .then(user => {
+            // console.log('User:', user)
+            // return  user
+            // })
+            return binding.query.user({where: {id: args.id}}, info)
+                .then(user => {
+                    console.log('User:', user)
+                    return user
+                })
+        }
+    }
+};
+
+const server =  new GraphQLServer({
+    typeDefs: `${__dirname}/schema.graphql`,
+    resolvers: resolvers
+});
+
+server.start().then(() => {
+    console.log('Server on http://localhost:4000');
+});
